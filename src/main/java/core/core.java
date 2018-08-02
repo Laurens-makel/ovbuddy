ddcc2c185e644a06b2c5122f7a5f885420e529b3
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 11:40:53 +0200

    Implement Database service in core class

--- src/main/java/core/core.java
+++ src/main/java/core/core.java
@@ -2,6 +2,7 @@ package core;
 
 import java.sql.*;
 
+import core.services.Database;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -10,9 +11,12 @@ import javafx.stage.Stage;
 import liquibase.exception.LiquibaseException;
 
 public class core extends Application {
+	private static final String JDBC_STRING = "jdbc:h2:~/database/hDOS2";
+	private Database database;
 	private Stage window;
 	
 	public static void main(String[] args) throws SQLException, LiquibaseException {
+		Database database = new Database(JDBC_STRING);
 		launch(args);
 	}
 	
