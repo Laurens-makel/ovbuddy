ed5dc74f66b1c294f451b37f8c46e5e179d6e19e
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 13:11:21 +0200

    Add JavaDoc to core class

--- src/main/java/core/core.java
+++ src/main/java/core/core.java
@@ -13,10 +13,30 @@ import javafx.stage.WindowEvent;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 
+/**
+* <h1>ovBuddy</h1>
+* This is a piece of software which retrieves your current location and nearby public transportation stops.
+* 
+* <i>ovBuddy helps you plan your travels</i>
+* 
+* @author  Laurens Mäkel
+* @version 0.0.1
+* @since   01-08-2018
+*/
+
 public class core extends Application {
 	private static final String JDBC_STRING = "jdbc:h2:~/database/hDOS2";
 	private Database database;
 	private Stage window;
+	
+	/**
+		* This is the main method which starts all services and launches GUI
+		* 
+		* @param args Unused
+		* @return Void
+		* @exception SQLException On database errors.
+		* @exception LiquibaseException On liquibase errors.
+	*/
 	
 	public static void main(String[] args) throws SQLException, LiquibaseException {
 		Database database = new Database(JDBC_STRING);
@@ -28,7 +48,6 @@ public class core extends Application {
 		
 		window.setOnCloseRequest(e -> {
 			try {
-				Platform.exit();
 				stop();
 			} catch (SQLException | DatabaseException err) {
 				System.out.println("An error occured while closing the database connection");
@@ -37,7 +56,17 @@ public class core extends Application {
 		});
 	}
 	
+	/**
+		* stop
+		* 
+		* Stops the application
+		* 
+		* @return Void
+		* @exception LiquibaseException On liquibase errors.
+	*/
+	
 	public void stop() throws SQLException, DatabaseException {
 		database.exit();
+		Platform.exit();
 	}
 }
