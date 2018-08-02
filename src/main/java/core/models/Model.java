6dcf95ce412e34b18d3c6ea429febe67b931812a
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 14:21:39 +0200

    Remove abstract property of model class

--- src/main/java/core/models/Model.java
+++ src/main/java/core/models/Model.java
@@ -3,9 +3,16 @@ package core.models;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Model {
-	private static List<Model> models = new ArrayList<Model>();
+/** Base class model
+ * Used as a base class for all models within this application
+ * 
+ * @author Laurens M�kel
+ * @version 0.0.1
+ * @since 02-08-2018
+*/
 
+public class Model {
+	private static List<Model> models = new ArrayList<Model>();
 	public Model() {
 		models.add(this);
 	}
