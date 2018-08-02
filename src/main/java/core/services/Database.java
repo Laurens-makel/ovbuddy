44a0aa2b5f57eed29e21b0d5d2f77fdf7f5ce87a
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 11:38:00 +0200

    Add database utility service

---
+++ src/main/java/core/services/Database.java
@@ -0,0 +1,48 @@
+package core.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+public class Database {
+	private static final String CHANGELOG = "DatabaseChangelog.xml";
+	private Connection conn;
+	private JdbcConnection connection;
+	private Liquibase liquibase;
+
+	public Database(String JdbcString) throws LiquibaseException, SQLException {
+		Connection conn = DriverManager.getConnection(JdbcString);
+		connection = new JdbcConnection(conn);
+		initialise();
+	}
+	
+	public Database(Connection c) throws LiquibaseException {
+		conn = c;
+		connection = new JdbcConnection(conn);
+		initialise();
+	}
+	
+	public Database(JdbcConnection c) throws LiquibaseException {
+		connection = c;
+		initialise();
+	}
+	
+	private void initialise() throws LiquibaseException {
+		ClassLoaderResourceAccessor resourceOpener = new ClassLoaderResourceAccessor();
+		
+		Liquibase liquibase = new Liquibase(CHANGELOG, resourceOpener, connection);
+		liquibase.validate();
+		liquibase.update(new Contexts());
+	}
+	
+	public void exit() throws SQLException {
+		conn.close();
+	}
+
+}
