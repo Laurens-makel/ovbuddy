011e12b283fa257d641089fe3118637c67716bdc
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 14:22:27 +0200

    Implement location support in model

--- src/main/java/core/models/Model.java
+++ src/main/java/core/models/Model.java
@@ -6,15 +6,25 @@ import java.util.List;
 /** Base class model
  * Used as a base class for all models within this application
  * 
- * @author Laurens Mï¿½kel
+ * @author Laurens Mäkel
  * @version 0.0.1
  * @since 02-08-2018
 */
 
 public class Model {
 	private static List<Model> models = new ArrayList<Model>();
+	private Location location;
+	
 	public Model() {
-		models.add(this);
+		Model.models.add(this);
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	public Location getLocation() {
+		return location;
 	}
 
 }
