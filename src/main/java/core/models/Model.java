3a7f96ce5213bec41987909e397d7bcb925fe3f2
INSIM\m63h114 <laurens@integrationpartners.nl>
2018-08-02 16:46:18 +0200

    Add JavaDoc to model class

--- src/main/java/core/models/Model.java
+++ src/main/java/core/models/Model.java
@@ -15,17 +15,55 @@ public class Model {
 	private static HashMap<String, Model> models = new HashMap<String, Model>();
 	private HashMap<String, Location> locations = new HashMap<String, Location>();
 	
+	/**
+		* Model
+		* 
+		* Registers new model with provided controllerName
+		* 
+		* @return Void
+	*/
+	
 	public Model(String controllerName) {
 		Model.models.put(controllerName, this);
 	}
+	
+	
+	/**
+		* addLocation
+		* 
+		* Registers a new location within the model with the provided identifier
+		* 
+		* @param String identifier
+		* @param Location location
+		* @return Void
+	*/
 	
 	public void addLocation(String identifier, Location location) {
 		locations.put(identifier, location);
 	}
 	
+	
+	/**
+		* getLocation
+		* 
+		* Retrieves a location within the model with the provided identifier
+		* 
+		* @param String identifier
+		* @return Location
+	*/
 	public Location getLocation(String identifier) {
 		return locations.get(identifier);
 	}
+	
+	
+	/**
+		* getController
+		* 
+		* Retrieves an instance of Model according to provided controllerName
+		* 
+		* @param String controllerName
+		* @return Model
+	*/
 	
 	public static Model getModel(String controllerName) {
 		return Model.models.get(controllerName);
