c9a918129ba5156d7409783f7d11ed9eb4f76cd6
MSI\Laurens <laurens@integrationpartners.nl>
2018-08-05 21:10:38 +0200

    Add static methods to location and add retrieving current location

--- src/main/java/core/models/Location.java
+++ src/main/java/core/models/Location.java
@@ -1,24 +1,57 @@
 package core.models;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.simple.parser.ParseException;
+
+import core.services.Geo;
+
 public class Location {
+	private static HashMap<String, Location> locations = new HashMap<String, Location>();
 	private String country;
 	private String city;
-	private String address;
 	private String zip;
+	private Double longitude;
+	private Double latitude;
 	
-	public Location(String city, String country, String address, String zip) {
+	public Location(String city, String country, String zip) {
 		this.city = city;
 		this.country = country;
-		this.address = address;
 		this.zip = zip;
+		
+		System.out.println(toString());
+		Location.locations.put(city +", " + zip, this);
+		// TODO: Add retrieving location longitude and latitude by provided info
 	}
 	
-	public Location() {
-		// TODO: Add retrieving current location
+	public Location() throws IOException, ParseException {
+		HashMap<String, HashMap> geoLocation = Geo.getLocationMap();
+
+		HashMap<String, Double> coordinates = geoLocation.get("coordinates");
+		this.latitude = coordinates.get("latitude");
+		this.longitude = coordinates.get("longitude");
+		
+		HashMap<String, String> details = geoLocation.get("details");
+		this.country = details.get("country");
+		this.city = details.get("city");
+		this.zip = details.get("zip");
+		
+		System.out.println(this.city +", " + this.zip);
+		Location.locations.put(this.city +", " + this.zip, this);
 	}
 	
 	public Location(int longitude, int latitude) {
 		// TODO: Add retrieving location according to provided longitude & latitude
+	}
+	
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
 	}
 	
 	
@@ -61,19 +94,21 @@ public class Location {
 	}
 	
 	
-	/**
-		* getAddress
-		* 
-		* Retrieves address of location
-		* 
-		* @return String
+	/**	
+	   * Retrieves latlong for current location
+	   * 
+	   * @return String latLong
 	*/
 	
-	public String getAddress() {
-		return address;
+	public String getLatlong(){
+		return latitude + "," + longitude;
+	}
+	
+	public static Location getLocation(String identifier) {
+		return locations.get(identifier);
 	}
 	
 	public String toString() {
-		return "This location is ["+city+"] in ["+country+"] with the address ["+address+"] and zip ["+zip+"]";
+		return "This location is ["+city+"] in ["+country+"] with the zip ["+zip+"] with latitude ["+latitude+"] and longitude ["+longitude+"]";
 	}
 }
