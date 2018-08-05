fca7990484ddbeae6c2ce4b4061085bb3077756e
MSI\Laurens <laurens@integrationpartners.nl>
2018-08-05 21:09:52 +0200

    Adjust home view and add handling input

--- src/main/java/core/controllers/Home.java
+++ src/main/java/core/controllers/Home.java
@@ -1,9 +1,20 @@
 package core.controllers;
 
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
 import core.models.Location;
 import core.models.Model;
+import core.services.Geo;
+import core.services.OV;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 /** Home controller
  * Used as controller for the Home screen
@@ -13,10 +24,48 @@ import javafx.scene.layout.GridPane;
  * @since 02-08-2018
 */
 public class Home extends Controller {
+	private Location current;
+	
+    @FXML
+    private TextField from;
 
-	public Home() {
+    @FXML
+    private TextField to;
+    
+    @FXML
+    private Button search;
+
+    @FXML
+    private Button saved;
+
+    @FXML
+    private VBox sideBox;
+
+    @FXML
+    private Button plan;
+
+    @FXML
+    void handleSearch(ActionEvent event) throws IOException, ParseException {
+    	String department = from.getText();
+    	String destination = to.getText();
+    	
+    	System.out.println("From ["+ department +"]");
+    	System.out.println("To ["+ destination +"]");
+    	
+    	System.out.println(OV.getReisplan(Location.getLocation(department), Geo.getLocation(destination) ));
+    } 
+
+	public Home() throws IOException, ParseException {
 		super("Home");
-		model.addLocation( "Current", new Location() );
+		
+		Controller.getModel().addLocation( "current", new Location() );
+		current = Controller.getModel().getLocation("current");
+	}
+
+	@FXML
+	public void initialize() {
+	    from.setText(current.getCity() +  ", " + current.getZip());
 	}
 
 }
+	
